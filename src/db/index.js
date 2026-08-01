4013493ab19231f31015054b6464418a5c0c01a8
oscar1-max <akanegbuosinachi@gmail.com>
2026-08-01 16:09:20 +0100

    Initialize database connection with schema setup

---
+++ src/db/index.js
@@ -0,0 +1,14 @@
+const Database = require("better-sqlite3");
+const fs = require("fs");
+const path = require("path");
+require("dotenv").config();
+
+const DB_PATH = process.env.DB_PATH || "./oscar_gold.db";
+const db = new Database(DB_PATH);
+db.pragma("journal_mode = WAL");
+db.pragma("foreign_keys = ON");
+
+const schema = fs.readFileSync(path.join(__dirname, "schema.sql"), "utf8");
+db.exec(schema);
+
+module.exports = db;
